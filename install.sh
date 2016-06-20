e96176813b5bf0369a74be10d0bd9302c03e68fb
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-06-21 04:19:44 +0700

    Include autotable.js file.

--- install.sh
+++ install.sh
@@ -4,9 +4,5 @@ if [ ! -d vendor/jsPDF ]; then
     mkdir vendor
     mv jsPDF-* vendor/jsPDF
     rm jsPDF.zip
-    wget --no-check-certificate -O jsPDF-AutoTable.zip https://github.com/simonbengtsson/jsPDF-AutoTable/archive/v2.0.25.zip
-    unzip jsPDF-AutoTable.zip
-    mv jsPDF-AutoTable-* jsPDF-AutoTable
-    rm jsPDF-AutoTable.zip
-    mv jsPDF-AutoTable/dist/jspdf.plugin.autotable.src.js vendor/jsPDF/plugins/autotable.js
+    mv autotable.js vendor/jsPDF/plugins/autotable.js
 fi
