16d98bca5e5f5429710e3767cfce46e5efed09ef
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-07-08 19:23:58 +0700

    Remove install script.

--- install.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -d vendor/jsPDF ]; then
-    wget --no-check-certificate -O jsPDF.zip https://github.com/MrRio/jsPDF/archive/v1.2.61.zip
-    unzip jsPDF.zip
-    mkdir vendor
-    mv jsPDF-* vendor/jsPDF
-    rm jsPDF.zip
-    mv autotable.js vendor/jsPDF/plugins/autotable.js
-fi
